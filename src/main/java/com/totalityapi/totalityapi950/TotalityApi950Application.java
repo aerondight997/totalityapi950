556ae31e79747681d3a2e6ad1512e39b20ab0435
aerondight997 <regisaerondight@gmail.com>
2024-03-23 22:43:41 +0100

    project start

---
+++ src/main/java/com/totalityapi/totalityapi950/TotalityApi950Application.java
@@ -0,0 +1,12 @@
+package com.totalityapi.totalityapi950;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TotalityApi950Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TotalityApi950Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/totalityapi/totalityapi950/TotalityApi950ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.totalityapi.totalityapi950;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TotalityApi950ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
