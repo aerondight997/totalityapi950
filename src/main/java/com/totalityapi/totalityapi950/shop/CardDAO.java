fbe73dfd460f39077b62eb1b73b09b7ad5ed06a9
aerondight997 <regisaerondight@gmail.com>
2024-03-24 19:50:36 +0100

    added class for testing

---
+++ src/main/java/com/totalityapi/totalityapi950/shop/CardDAO.java
@@ -0,0 +1,4 @@
+package com.totalityapi.totalityapi950.shop;
+
+public class CardDAO {
+}
---
+++ src/main/java/com/totalityapi/totalityapi950/shop/ShopService.java
@@ -0,0 +1,16 @@
+package com.totalityapi.totalityapi950.shop;
+
+import com.totalityapi.totalityapi950.shared.ShopInterface;
+
+public class ShopService implements ShopInterface {
+
+    @Override
+    public void calculateCurrency() {
+
+    }
+
+    @Override
+    public void purchaseItem() {
+
+    }
+}
