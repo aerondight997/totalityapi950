35849d054e924a36c95bfe4e86cfd54387f1db19
aerondight997 <regisaerondight@gmail.com>
2024-03-24 11:45:44 +0100

    added class for testing

--- src/main/java/com/totalityapi/totalityapi950/speedrun/RoundDAO.java
+++ src/main/java/com/totalityapi/totalityapi950/speedrun/RoundDAO.java
@@ -1,4 +1,16 @@
 package com.totalityapi.totalityapi950.speedrun;
 
 public class RoundDAO {
+    public void getCorrectSelectionCount() {
+
+    }
+
+    public void getIncorrectSelectionCount() {
+
+    }
+
+    public void getRoundDuration() {
+
+    }
+
 }
