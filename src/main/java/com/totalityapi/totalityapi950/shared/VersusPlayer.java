05fd48fa14af17ea28f526c5650090edc00ac17e
aerondight997 <regisaerondight@gmail.com>
2024-03-27 19:50:41 +0100

    fixes to algorithm and matchmaking

--- src/main/java/com/totalityapi/totalityapi950/shared/VersusPlayer.java
+++ src/main/java/com/totalityapi/totalityapi950/shared/VersusPlayer.java
@@ -1,4 +1,5 @@
 package com.totalityapi.totalityapi950.shared;
 
 public class VersusPlayer {
+    public String searchStatus;
 }
--- src/main/java/com/totalityapi/totalityapi950/versusroom/VersusSearchMatchmakingService.java
+++ src/main/java/com/totalityapi/totalityapi950/versusroom/VersusSearchMatchmakingService.java
@@ -3,7 +3,8 @@ package com.totalityapi.totalityapi950.versusroom;
 public enum VersusSearchMatchmakingService implements VersusSearchMatchmakingInterface {
     IDLE("Player is not searching or match wasn't found"),
     SEARCHING("Player is searching for a match"),
-    FOUND("Player has found the match");
+    FOUND("Player has found the match"),
+    ;
 
     final String searchStatus;
 
@@ -13,16 +14,19 @@ public enum VersusSearchMatchmakingService implements VersusSearchMatchmakingInt
 
     @Override
     public void setSearchStatusToIdle() {
-
+     //this method is called on fetched versus lobby with no action and on STOP SEARCH COMPONENT ON FRONT END
+        // save the status to the database!!!!
     }
 
     @Override
     public void setSearchStatusToSearching() {
-
+     //this is called on START SEARCH COMPONENT
+        // implements SearchAlgorithm
+        // this should replace the value of IDLE with SEARCHING in database
     }
 
     @Override
     public void setSearchStatusToFound() {
-
+      // THIS IS CALLED IF CONNECTION IS ESTABLISHED
     }
 }
