e506effd6797211624851c93208630952693d665
aerondight997 <regisaerondight@gmail.com>
2024-03-28 09:23:16 +0100

    classes rearranged.

--- src/main/java/com/totalityapi/totalityapi950/shared/ValidationInterface.java
+++ src/main/java/com/totalityapi/totalityapi950/shared/ValidationInterface.java
@@ -2,4 +2,5 @@ package com.totalityapi.totalityapi950.shared;
 
 public interface ValidationInterface {
     void validateSelection();
+
 }
--- src/main/java/com/totalityapi/totalityapi950/speedrun/SpeedrunValidationService.java
+++ src/main/java/com/totalityapi/totalityapi950/speedrun/SpeedrunValidationService.java
@@ -3,10 +3,9 @@ package com.totalityapi.totalityapi950.speedrun;
 import com.totalityapi.totalityapi950.shared.ValidationInterface;
 
 public class SpeedrunValidationService implements ValidationInterface {
+
     @Override
     public void validateSelection() {
-       //  compare images from respective containers,
-        // if image is equal to the other image, call SpeedrunDAO set methods for
-        // selection counts
+
     }
 }
